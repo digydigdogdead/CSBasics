840fdd130cc348142000560f2c1fc41587a0e4c8
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-05 16:28:43 +0000

    Added a BankingSystem class that currently has a method that creates accounts and a method that lists all accounts

---
+++ BankingSystem/ConsoleApp1/ConsoleApp1/BankingSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BankingSystem
+    {
+        public List<BankAccount> AllAcounts = new List<BankAccount>();
+        public BankAccount CreateAccount(int accountNumber, int balance, string ownerName, int agreedOverdraft)
+        {
+            BankAccount newAccount = new BankAccount(accountNumber, balance, ownerName, agreedOverdraft);
+            AllAcounts.Add(newAccount);
+            return newAccount;
+        }
+
+        public void ListAccounts()
+        {
+            foreach (BankAccount account in AllAcounts)
+            {
+                Console.WriteLine(account.AccountNumber);
+            }
+        }
+    }
+}
--- BankingSystem/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ BankingSystem/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4991f59e914d4bdb39c0dfa9affc3ee77802123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0588cd2abc65675d6ce83a126b9164dd02ec7a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
